53ad94cfbed08d2849d54f8f1e8e034eaed6a10e
Alyssa Lubrano <aly.developer1@gmail.com>
2025-03-26 18:23:12 +1100

    Added some projects

--- my-react-app/src/components/Projects.js
+++ my-react-app/src/components/Projects.js
@@ -14,12 +14,12 @@ import Paper from '@mui/material/Paper';
 const Projects = () => {
 
     const projects = [
-        {name: 'a', description: 'testing a blurb for this project', height: '20vh'},
-        {name: 'b', description: 'testing a blurb for this project', height: '8vh'},
-        {name: 'c', description: 'testing a blurb for this project', height: '14vh'},
-        {name: 'd', description: 'testing a blurb for this project', height: '12vh'},
-        {name: 'e', description: 'testing a blurb for this project', height: '14vh'},
-        {name: 'f', description: 'testing a blurb for this project', height: '15vh'},
+        {name: 'This Website!', description: 'ta da! :)', height: '20vh'},
+        {name: 'Productivity App', description: 'This app combines all the best parts of apps like Notion, ToDolist and Lofi. A productivity hub for academic and life purposes. Coming soon ;)', height: '8vh'},
+        {name: 'Dungeonmania', description: 'This is a dungeon crawler game I created a few years ago for a uni course, to apply what I learnt about Object Oriented Programming in Java.', height: '14vh'},
+        {name: 'VHDL Basics', description: 'A collation of all tasks from my COMP3222 VHDL class at UNSW. It includes fun stuff like a HiLo guessing game and a binary to hexadecimal switch/led system. All written in VHDL :)', height: '12vh'},
+        {name: 'Wacky Microphone', description: 'This is a program that reads data from an i2s microphone and converts it into a .wav file. It has a UI where you can apply various effects to this .wav file. The audio processing is written in Java, the effects application written in C, and the UI is written in Node.js', height: '14vh'},
+        {name: 'AirBrb', description: 'This is an AirBnb dupe for COMP6080 at UNSW. It is a React app.', height: '15vh'},
         {name: 'g', description: 'testing a blurb for this project', height: '20vh'},
         {name: 'h', description: 'testing a blurb for this project', height: '8vh'},
         {name: 'i', description: 'testing a blurb for this project', height: '10vh'},
