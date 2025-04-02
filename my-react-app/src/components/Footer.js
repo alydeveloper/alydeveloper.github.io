e2d05cd3ba07ca625c83cdcdc3efa887f34719e4
Alyssa Lubrano <aly.developer1@gmail.com>
2025-04-02 16:41:59 +1100

    Added a footer

--- my-react-app/src/components/Footer.js
+++ my-react-app/src/components/Footer.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import { Box } from '@mui/material';
+
 const Footer = () => {
     return (
-        <>
-            YIPPEE
-        </>
+        <Box sx={{ padding: '2vh', display: 'flex', justifyContent: 'center' }}>
+            ♡ Made by Alyssa Lubrano in 2025 ♡
+        </Box>
     );
 };
 
